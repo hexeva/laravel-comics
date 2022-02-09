4a4b30dda61ef01b23c40a9dd3e9aa7fb4e7b46b
hexeva <patrick79.g@gmail.com>
2022-02-09 15:50:05 +0100

    created header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,49 @@
 <header>
-    sono l'header pippo
+   
+    <div class="header-top">
+        
+    </div>
+    
+    <div class="container">
+        <div class="left-col">
+            <div class="logo">
+                <img src="{{ asset('images/dc-logo.png') }}" alt="logo dc">
+            </div>
+        </div>
+        <div class="right-col">
+            <ul>
+                <li>
+                    <a href="">characters</a>
+                </li>
+                <li>
+                    <a href="">comics</a>
+                </li>
+                <li>
+                    <a href="">movies</a>
+                </li>
+                <li>
+                    <a href="">tv</a>
+                </li>
+                <li>
+                    <a href="">games</a>
+                </li>
+                <li>
+                    <a href="">collectibles</a>
+                </li>
+                <li>
+                    <a href="">videos</a>
+                </li>
+                <li>
+                    <a href="">fans</a>
+                </li>
+                <li>
+                    <a href="">news</a>
+                </li>
+                <li>
+                    <a href="">shop</a>
+                </li>
+            </ul>
+        </div>
+
+    </div>
 </header>
