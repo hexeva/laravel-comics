1c67fa7ffebe64a74e5f5e91a4f5faec0ff522f1
hexeva <patrick79.g@gmail.com>
2022-02-10 08:28:50 +0100

    added footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,146 @@
 <footer>
-    sono il footer pippo
+    <div class="footer-top">
+        <div class="container">
+              <!-- links section -->
+                <div class="links-section">
+                    <div class="links">
+                        <h2>dc comics</h2>
+                        <ul>
+                            <li>
+                                <a href="#">Characters</a>
+                            </li>
+                            <li>
+                                <a href="#">Comics</a>
+                            </li>
+                            <li>
+                                <a href="#">Movies</a>
+                            </li>
+                            <li>
+                                <a href="#">TV</a>
+                            </li>
+                            <li>
+                                <a href="#">Games</a>
+                            </li>
+                            <li>
+                                <a href="#">Videos</a>
+                            </li>
+                            <li>
+                                <a href="#">News</a>
+                            </li>
+                        </ul>
+                        <h2>shop</h2>
+                        <ul>
+                            <li>
+                                <a href="#">Shop DC</a>
+                            </li>
+                            <li>
+                                <a href="#">Shop DC Collectibles</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- end links -->
+                    <div class="links">
+                        <h2>dc</h2>
+                        <ul>
+                            <li>
+                                <a href="#">Terms of Use</a>
+                            </li>
+                            <li>
+                                <a href="#">Privacy policy (New)</a>
+                            </li>
+                            <li>
+                                <a href="#">Ad choices</a>
+                            </li>
+                            <li>
+                                <a href="#">Advertising</a>
+                            </li>
+                            <li>
+                                <a href="#">Jobs</a>
+                            </li>
+                            <li>
+                                <a href="#">Subscriptions</a>
+                            </li>
+                            <li>
+                                <a href="#">Talent Workshops</a>
+                            </li>
+                             <li>
+                                <a href="#">CPSC Certificated</a>
+                            </li>
+                            <li>
+                                <a href="#">Rating</a>
+                            </li>
+                            <li>
+                                <a href="#">Shop Help</a>
+                            </li>
+                            <li>
+                                <a href="#">Contact Us</a>
+                            </li>
+                        </ul>
+                        
+                    </div>
+                    <!-- end links -->
+                    <div class="links">
+                        <h2>sites</h2>
+                        <ul>
+                            <li>
+                                <a href="#">DC</a>
+                            </li>
+                            <li>
+                                <a href="#">MAD Magazine</a>
+                            </li>
+                            <li>
+                                <a href="#">DC Kids</a>
+                            </li>
+                            <li>
+                                <a href="#">Dc Universe</a>
+                            </li>
+                            <li>
+                                <a href="#">DC Power Visa</a>
+                            </li>
+                            
+                        </ul>
+                       
+                    </div>
+                    <!-- end links -->
+                </div>
+
+                <!-- end links sections -->
+                <div class="dc_logo">
+                    <img src="../images/dc-logo-bg.png" alt="dc logo">
+                </div>
+        </div>
+            <!-- end container -->      
+    </div>
+    <!-- END FOOTER TOP -->
+    
+    {{--FOOTER BOTTOM  --}}
+    <section>
+        <div class="container">
+            <div class="footer-btn">
+                <a href="#">SIGN-UP-NOW</a>
+            </div>
+            <div class="links">
+                <ul>
+                    <li>
+                        <a href="#">FOLLOW US</a>
+                    </li>
+                    <li>
+                        <img src="{{asset('images/footer-facebook.png')}}" alt="facebook logo">
+                    </li>
+                    <li>
+                        <img src="{{asset('images/footer-twitter.png')}}" alt="twitter logo">
+                    </li>
+                    <li>
+                        <img src="{{asset('images/footer-youtube.png')}}" alt="youtube logo">
+                    </li>
+                    <li>
+                        <img src="{{asset('images/footer-pinterest.png')}}" alt="pinterest logo">
+                    </li>
+                    <li>
+                        <img src="{{asset('images/footer-periscope.png')}}" alt="periscopo logo">
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
 </footer>
